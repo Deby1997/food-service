6b4df41166a2f866787b220498ffdbe8094ab317
Deby1997 <debysarker159@gmail.com>
2022-04-18 19:17:41 +0600

    About detail added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import image from '../../images/IMG20201022201031.jpg';
 
 const About = () => {
     return (
         <div>
-
+            <div class="card w-25 text-center" >
+                <img src={image} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5>Name:Deby Sarker</h5>
+                    <p class="card-text">I want to be a professional web developer in future.</p>
+                </div>
+            </div>
         </div>
     );
 };
